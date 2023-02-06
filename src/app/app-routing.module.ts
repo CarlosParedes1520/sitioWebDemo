6f2222b9c12a5c4e49a8927b89e9fb31b060c82c
Carlos Paredes <noelita6411@hotmail.com>
2023-02-06 15:02:36 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+import { PagesRoutingModule } from './pages/pages-routing.module';
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+  },
+  {
+    path: '',
+    loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule)
+  },
+  {
+    path: '**', 
+    component: NopagefoundComponent
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes),
+    // PagesRoutingModule
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
